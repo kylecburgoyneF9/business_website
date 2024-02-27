963e5ba38d6d2dcd869dc07d9e1b25a2382cd322
kylecburgoyneF9 <kylecburgoyne@gmail.com>
2024-02-26 18:42:53 -0700

    Footer and Navbar Additions

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+<template>
+    <footer class="footer">
+        <div class="footer-text">
+            <p></p>
+        </div>
+
+        <div class="footer-iconTop">
+            <a href="#"><box-icon name='up-arrow-alt' type='solid'></box-icon></a>
+        </div>
+    </footer>
+</template>
+<style scoped></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+    <header class="header" :class="{ 'sticky': isSticky }">
+        <a href="/" class="logo">KB | <span>CharacterMatters</span></a>
+        <div class="menu-icon" @click="toggleMenu">
+            <box-icon name='menu'></box-icon>
+        </div>
+        <nav class="navbar" :class="{ 'active': isMenuOpen }">
+            <router-link to="/consulting">Consulting</router-link>
+            <router-link to="/coaching">Coaching</router-link>
+            <router-link to="/partners">Partners</router-link>
+            <router-link to="#about">Reviews</router-link>
+            <router-link to="/contact">Contact</router-link>
+        </nav>
+    </header>
+</template>
+
+<style scoped></style>
+
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+import { RouterLink } from 'vue-router';
+
+const isMenuOpen = ref(false);
+
+const toggleMenu = () => {
+    isMenuOpen.value = !isMenuOpen.value;
+};
+
+const isSticky = ref(false);
+
+const handleScroll = () => {
+    isSticky.value = window.scrollY > 100; // Adjust the threshold as needed
+};
+
+onMounted(() => {
+    window.addEventListener('scroll', handleScroll);
+});
+
+onBeforeUnmount(() => {
+    window.removeEventListener('scroll', handleScroll);
+});
+</script>

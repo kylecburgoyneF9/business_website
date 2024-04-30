7b0b4239822cfdb58ee6683a3b4276c687f73eb3
kylecburgoynef9 <kylecburgoyne@gmail.com>
2024-04-30 00:00:06 -0700

    Update Reviews.vue

--- src/components/Reviews.vue
+++ src/components/Reviews.vue
@@ -1,5 +1,5 @@
 <template>
-      <!-- 'reviews' section design and elements -->
+  <!-- 'reviews' section design and elements -->
   <section class="reviews" id="reviews">
     <h2 class="heading">My <span>Reviews</span></h2>
     <div class="review-cards-container">
@@ -9,16 +9,21 @@
         </div>
         <div class="review-card-text">
           <h3>Glenn McClelland - Retired CEO / CFO</h3>
-          <p>"I worked with Kyle in a start-up effort for a solar services company.  Kyle is one of those rare individuals who can effectively analyze a situation and provide their own direction to do what needs to be done.  Kyle is intelligent, self-motivated and a problem solver who understands the relationship between responsibility and accountability!  With Kyle's ambition and ability to work as part of a team, I would recommend Kyle to any organization looking for a manager with virtually unlimited upside potential.  "</p>
+          <p>"I worked with Kyle in a start-up effort for a solar services company. Kyle is one of those rare
+            individuals who can effectively analyze a situation and provide their own direction to do what needs to be
+            done. Kyle is intelligent, self-motivated and a problem solver who understands the relationship between
+            responsibility and accountability! With Kyle's ambition and ability to work as part of a team, I would
+            recommend Kyle to any organization looking for a manager with virtually unlimited upside potential. "</p>
         </div>
       </div>
       <div class="review-card-contents">
         <div class="review-card-img">
-          <img src="../assets/Home/Jeff_Image.jpeg" alt="">
+          <img src="../assets/Home/Self.jpeg" alt="">
         </div>
         <div class="review-card-text">
           <h3>Jeff Bunge - </h3>
-          <p>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas commodi asperiores doloremque perspiciatis
+          <p>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas commodi asperiores doloremque
+            perspiciatis
             voluptatum sunt totam esse at repudiandae autem!"</p>
         </div>
       </div>
@@ -28,7 +33,8 @@
         </div>
         <div class="review-card-text">
           <h3>Name and Title</h3>
-          <p>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas commodi asperiores doloremque perspiciatis
+          <p>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas commodi asperiores doloremque
+            perspiciatis
             voluptatum sunt totam esse at repudiandae autem!"</p>
         </div>
       </div>
@@ -73,17 +79,15 @@
   filter: grayscale(100%);
 }
 
-.review-cards-container .review-card-contents:nth-of-type(2) img {
+.review-cards-container .review-card-contents:nth-of-type(2) img {}
 
-}
-
-.review-cards-container .review-card-contents:nth-of-type(3) img {
-
-}
+.review-cards-container .review-card-contents:nth-of-type(3) img {}
 
 .review-cards-container .review-card-contents .review-card-img img {
-  width: 18rem; /* Adjust the width as needed */
-  height: 18rem; /* Adjust the height as needed */
+  width: 18rem;
+  /* Adjust the width as needed */
+  height: 18rem;
+  /* Adjust the height as needed */
   border-radius: 50%;
   border: var(--dk-blue) solid;
   object-fit: cover;
@@ -99,10 +103,10 @@
 }
 
 @media (max-width: 700px) {
-    .review-cards-container .review-card-contents  {
-        flex-direction: column;
-        text-align: center;
-        padding: 20px;
-    }
+  .review-cards-container .review-card-contents {
+    flex-direction: column;
+    text-align: center;
+    padding: 20px;
+  }
 }
 </style>
